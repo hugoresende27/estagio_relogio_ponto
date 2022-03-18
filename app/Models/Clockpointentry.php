4adae9827682cd4ef9ed97c964bae2b7a5b5f939
hugoresende27 <hugoresende27@gmail.com>
2022-03-18 10:13:15 +0000

    clockpointentry

---
+++ app/Models/Clockpointentry.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Traits\Tenantable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Clockpointentry extends Model
+{
+    use HasFactory;
+    use Tenantable;
+
+    protected $guarded = ['id'];
+}
---
+++ database/migrations/2022_03_18_095544_create_clockpointentries_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clockpointentries', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('tenant_id')->nullable()->index();
+            $table->foreign('tenant_id')->references('id')->on('tenants')->onDelete('set null');
+
+            $table->unsignedBigInteger('employee_id')->nullable()->index();
+            $table->foreign('employee_id')->references('id')->on('employees')->onDelete('set null');
+
+            // $table->timestamps('clock_in','clock_out');
+            $table->datetime ('clock_in');
+            $table->datetime ('clock_out');
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clockpointentries');
+    }
+};
